02e3540c48f81102e742203a5179da803ee1f979
Felix Palmer <felixpalmer@gmail.com>
2014-01-13 14:56:08 +0100

    Shader validation

--- glsl-validate.py
+++ glsl-validate.py
@@ -7,13 +7,44 @@ import subprocess
 
 DIR=os.path.dirname(os.path.realpath(__file__))
 
-# Load in prefix files, see the prefix directory for examples
-VERTEX_PREFIX = ""
-FRAGMENT_PREFIX = ""
-with open(os.path.join(DIR, "prefix/prefix.vert"), 'r') as f:
-    VERTEX_PREFIX = f.read()
-with open(os.path.join(DIR, "prefix/prefix.frag"), 'r') as f:
-    FRAGMENT_PREFIX = f.read()
+# Color terminal output
+def color(s, color):
+    return "\033[1;%dm%s\033[1;m" % (color, s)
+
+def red(s):
+    return color(s, 31)
+
+def grey(s):
+    return color(s, 30)
+
+def validate_shader(shader_file):
+    extension = os.path.splitext(shader_file)[1]
+    tmp_file_name = "tmp%s" % extension
+
+    # Load in the prefix for the shader first and then append the actual shader
+    shutil.copyfile(os.path.join(DIR, "prefix/prefix%s" % extension), os.path.join(DIR, tmp_file_name))
+    with open(shader_file, 'r') as f:
+        shader = f.read()
+    with open(os.path.join(DIR, tmp_file_name), 'a') as f:
+        f.write(shader)
+
+    # Run essl_to_glsl over the shader, reporting any errors
+    p = subprocess.Popen([os.path.join(DIR, "angle/essl_to_glsl_osx"),
+                          "-s=w",
+                          "-x=d",
+                          os.path.join(DIR, tmp_file_name)],
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.STDOUT)
+    ret_code = p.wait()
+    os.remove(os.path.join(DIR, tmp_file_name))
+
+    if ret_code != 0:
+        header = "ERROR in %s:" % shader_file
+        error = red(header + "\n")
+        error += grey(len(header) * "=" + "\n")
+        error += "".join(p.stdout.readlines()[1:-4])
+        print error
+        exit(1)
 
 def standalone():
     parser = argparse.ArgumentParser(description='Validate three.js shaders')
@@ -25,6 +56,8 @@ def standalone():
     for f in bad_extensions:
         print "Invalid file: %s, only support .frag and .vert files" % f
         exit(1)
+
+    map(validate_shader, files)
 
 if __name__ == "__main__":
     standalone()
