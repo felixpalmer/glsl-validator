0498839dbc5b0d04492db8a164cf1fb2c6aa45d4
Felix Palmer <felixpalmer@gmail.com>
2016-05-23 21:56:27 +0200

    Reformat python code

--- glsl-validate.py
+++ glsl-validate.py
@@ -3,10 +3,9 @@ import argparse
 import os
 import platform
 import re
-import shutil
 import subprocess
 
-DIR=os.path.dirname(os.path.realpath(__file__))
+DIR = os.path.dirname(os.path.realpath(__file__))
 
 # Select the correct essl_to_glsl executable for this platform
 if platform.system() == 'Darwin':
@@ -19,7 +18,11 @@ else:
     print "Unsupported platform"
     exit(1)
 
+# See README for where to obtain
+CGC = os.path.join(DIR, "cgc")
+
 args = {}
+
 
 # Color terminal output
 def color(s, color):
@@ -28,12 +31,15 @@ def color(s, color):
     else:
         return s
 
+
 def grey(s):
     return color(s, 30)
 
+
 def load_shader(shader_file):
     output = ""
-    # Keep track of line numbers, #including will result in some corresponding to other files
+    # Keep track of line numbers, #including will result in some corresponding
+    # to other files
     line_labels = []
     with open(shader_file, 'r') as f:
         line_num = 1
@@ -41,7 +47,8 @@ def load_shader(shader_file):
             include_match = re.match("#include (.*)", line)
             if include_match:
                 include_file = include_match.group(1)
-                fullpath = os.path.join(os.path.dirname(shader_file), include_file)
+                fullpath = os.path.join(os.path.dirname(shader_file),
+                                        include_file)
                 (included_shader, included_line_labels) = load_shader(fullpath)
                 output += included_shader
                 line_labels += included_line_labels
@@ -51,7 +58,8 @@ def load_shader(shader_file):
             line_num += 1
     return (output, line_labels)
 
-def validate_shader(shader_file):
+
+def create_tmp_file(shader_file):
     extension = os.path.splitext(shader_file)[1]
     tmp_file_name = "tmp%s" % extension
 
@@ -69,13 +77,16 @@ def validate_shader(shader_file):
     with open(os.path.join(DIR, tmp_file_name), 'w') as f:
         f.write(shader)
 
+    return (tmp_file_name, line_labels)
+
+
+def validate_shader(shader_file):
+    (tmp_file_name, line_labels) = create_tmp_file(shader_file)
     # Run essl_to_glsl over the shader, reporting any errors
-    p = subprocess.Popen([ESSL_TO_GLSL,
-                          "-s=w",
-                          "-x=d",
+    p = subprocess.Popen([ESSL_TO_GLSL, "-s=w", "-x=d",
                           os.path.join(DIR, tmp_file_name)],
-                          stdout=subprocess.PIPE,
-                          stderr=subprocess.STDOUT)
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
     ret_code = p.wait()
     os.remove(os.path.join(DIR, tmp_file_name))
 
@@ -95,19 +106,25 @@ def validate_shader(shader_file):
         print error
         exit(1)
 
+
 def standalone():
     parser = argparse.ArgumentParser(description='Validate three.js shaders')
     parser.add_argument('files', metavar='FILE', type=str, nargs='+',
-                               help='files to validate')
-    parser.add_argument('--color', dest='color', action='store_true', help='Color output')
-    parser.add_argument('--no-color', dest='color', action='store_false', help='Color output')
-    parser.add_argument('--raw', dest='raw', action='store_true', help='Do not prepend standard THREE.js prefix block (useful for RawShaderMaterial)')
-    parser.add_argument('--write', dest='write', action='store_true', help='Write out to file.full.ext')
+                        help='files to validate')
+    parser.add_argument('--color', dest='color', action='store_true',
+                        help='Color output')
+    parser.add_argument('--no-color', dest='color', action='store_false',
+                        help='Color output')
+    parser.add_argument('--raw', dest='raw', action='store_true',
+                        help='Do not prepend standard THREE.js prefix block')
+    parser.add_argument('--write', dest='write', action='store_true',
+                        help='Write out to file.full.ext')
     parser.set_defaults(color=True)
     global args
     args = parser.parse_args()
     files = args.files
-    bad_extensions = filter(lambda f: not re.match('^\.(vert|frag)$', os.path.splitext(f)[1]), files)
+    bad_extensions = filter(lambda f: not re.match('^\.(vert|frag)$',
+                            os.path.splitext(f)[1]), files)
     for f in bad_extensions:
         print "Invalid file: %s, only support .frag and .vert files" % f
         exit(1)
@@ -116,12 +133,12 @@ def standalone():
 
     if args.write:
         for f in files:
-            dest_name = f.split( '.' )
-            dest_name.insert( -1, 'full' )
-            dest_name = ".".join( dest_name )
-            with open( dest_name, 'w' ) as out:
-                (shader, lines) = load_shader( f ) 
-                out.write( shader )
+            dest_name = f.split('.')
+            dest_name.insert(-1, 'full')
+            dest_name = ".".join(dest_name)
+            with open(dest_name, 'w') as out:
+                (shader, lines) = load_shader(f)
+                out.write(shader)
 
 if __name__ == "__main__":
     standalone()
--- prefix/prefix.frag
+++ prefix/prefix.frag
@@ -1,3 +1,4 @@
+#extension GL_OES_standard_derivatives : enable
 precision highp float;
 precision highp int;
 uniform mat4 viewMatrix;
